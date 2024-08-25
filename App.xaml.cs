68a66420b2caf3c816a1df9bbdf03249b7ae336c
WillianDiazFS <willian.diaz@farsiman.com>
2024-08-24 23:49:25 -0600

    initial

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SupportDesk.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
